d761e913f157e3826cdf9f4e1b1ce1f3bb78bf62
Jonathan Krall <jonathan.krall@lexicon.se>
2026-07-15 13:49:44 +0200

    Added code to add all new users to the User-role

--- IdentityDemo2026/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ IdentityDemo2026/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -135,6 +135,9 @@ public class RegisterModel : PageModel
             {
                 _logger.LogInformation("User created a new account with password.");
 
+                //Code to add all new accounts to user role
+                //await _userManager.AddToRoleAsync(user, "User"); 
+
                 var userId = await _userManager.GetUserIdAsync(user);
                 var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                 code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
