6e87ebdb2d47f1a986dc0f4d26848c03aa2d542b
Jonathan Krall <jonathan.krall@lexicon.se>
2026-07-15 14:12:22 +0200

    Added some views and checks to limit access based on roles

---
+++ IdentityDemo2026/Controllers/IdentityDemoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityDemo2026.Controllers
+{
+    [Authorize]
+    public class IdentityDemoController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Authorize(Roles ="Admin")]
+        public IActionResult Index2()
+        {
+            return View();
+        }
+        public IActionResult Index3()
+        {
+            return View();
+        }
+    }
+}
