7bd3c7c10fb7e909d6a69e83d8f0c478f200bfaa
Jonathan Krall <jonathan.krall@lexicon.se>
2026-07-15 11:45:41 +0200

    Fixed the issues added by Scaffolding

--- IdentityDemo2026/Data/ApplicationDbContext.cs
+++ IdentityDemo2026/Data/ApplicationDbContext.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace IdentityDemo2026.Data
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
     }
 }
