cbd39c2299f7510a3e4293737807795e9c6f0053
Jonathan Krall <jonathan.krall@lexicon.se>
2026-07-15 11:33:57 +0200

    Add project files.

---
+++ IdentityDemo2026/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IdentityDemo2026.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
